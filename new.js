b28e0696f391c95a344ecac9096ae13e55ebf03f
YousufSH <160315514+ShaikhYousuf39@users.noreply.github.com>
2024-03-10 01:19:31 +0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,6 @@
+var alien_color = "green";
+if (alien_color == "green")
+    console.log("alien is green");
+var alien_color2 = "red";
+if (alien_color2 == "green")
+    console.log("alien is red");
---
+++ new.ts
@@ -0,0 +1,9 @@
+let alien_color : string = "green"
+
+if (alien_color == "green")
+console.log("you earn 5 points")
+
+let alien_color2 : string = "red"
+
+if (alien_color2 == "green")
+console.log("no output")
